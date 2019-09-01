9dae1c9c17b56b034123f90af14f8149dcc3b82d
S1m0nZhu <zm349@qq.com>
2019-09-01 18:26:12 +0800

    引用与借用

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
-struct Point
-{
-    x: f64,
-    y: f64
-}
-struct Line
-{
-    start: Point,
-    end: Point,
+//fn main() {
+//    let s1 = String::from("hello");
+//
+//    let len = calculate_length(&s1);
+//
+//    println!("The length of '{}' is {},", s1, len);
+//}
+//
+//fn calculate_length(s:&String) -> usize { //&引用， ×解引用，获取引用作为函数参数称为借用
+//    s.len()
+//}
+
+fn main() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
 }
 
-fn structures()
-{
-    let p = Point { x:3.0, y:4.0};
-    println!("point p is at ({},{})", p.x, p.y);
-    let p2 = Point { x: 5.0, y: 10.0};
-    let myline = Line { start: p, end: p2 };
-}
-fn main() {
-    structures();
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+    println!("{}", some_string)
 }
