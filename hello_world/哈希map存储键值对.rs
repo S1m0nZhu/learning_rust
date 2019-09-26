be0df865e8dd067acd8f67c168e2ff43aba6bf70
S1m0nZhu <zm349@qq.com>
2019-09-26 22:41:05 +0800

    HashMap

--- hello_world/哈希map存储键值对.rs
+++ hello_world/哈希map存储键值对.rs
@@ -1,1 +1,16 @@
-'
+//可以使用new创建一个空的HashMap，并使用insert增加元素
+//记录两支队伍的分数，蓝队10分，黄队50分
+use std::collections:HashMap;
+
+let mut scores = HashMap::new();
+
+scores.insert(String::from("Blue"), 10);
+scores.insert(String::from("Yellow"), 50);
+
+//与vector一样，哈希map将数据存储在堆上，这个HashMap的了；类型是String而值类型是i32
+use std::collections:HashMap;
+
+let teams = vec![String::from("Blue"), String::from("Yellow")];
+let initial_scores = vec![10, 50];
+
+let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
